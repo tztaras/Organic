486b3442d4283150ae587b1ceae369d46c4593ea
tztaras <tztaras@gmail.com>
2024-08-28 15:05:06 +0300

    3rd

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const menuIcon = document.querySelector('.menu-icon');
 console.log(menuIcon);      
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('menu-acon__active');
+    console.log(menuIcon); 
+})
